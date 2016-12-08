ac364d843c64dd8022126b0e350773e8bba6a55c
codematrix <ross_pellegrino@hotmail.com>
2016-12-08 14:59:26 -0500

    Added new ListButton

---
+++ src/ReflectSoftware.Facebook.Messenger.Common/Models/ListButton.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace ReflectSoftware.Facebook.Messenger.Common.Models
+{
+    public class ListButton : Button
+    {
+        public ListButton(string type) : base(type)
+        {
+        }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("messenger_extensions")]
+        public bool MessengerExtensions { get; set; }
+
+        [JsonProperty("webview_height_ratio")]
+        public string WebviewHeightRatio { get; set; }
+
+        [JsonProperty("fallback_url")]
+        public string FallbackUrl { get; set; }
+    }
+}
---
+++ src/ReflectSoftware.Facebook.Messenger.Common/Models/ListElement.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace ReflectSoftware.Facebook.Messenger.Common.Models
+{
+    public class ListElement : GenericElement
+    {
+        [JsonProperty("default_action")]
+        public ListButton DefaultAction { get; set; }
+    }
+}
